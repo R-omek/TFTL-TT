ce4ad102eda374198f207bf1ce43a6abc188d290
roman hrytsiuk <rmngrck97@gmail.com>
2024-12-16 11:07:11 +0200

    initial commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+</script>
+<template>
+    <div class="home-page">
+
+    </div>
+</template>
+<style scoped lang='scss'>
+    .home-page {
+        background: #bebebe;
+        width: 100vw;
+        height: 100vh;
+    }
+</style>
---
+++ src/pages/SomePage.vue
@@ -0,0 +1,7 @@
+<script setup lang='ts'>
+</script>
+<template>
+    <h2>Some page</h2>
+</template>
+<style scoped lang='scss'>
+</style>
