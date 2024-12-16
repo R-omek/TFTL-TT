10b951cd8518835b21cc5e756b08800822a440a0
roman hrytsiuk <rmngrck97@gmail.com>
2024-12-16 15:08:24 +0200

    add reverse rotated text

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,6 +3,7 @@ import BgText from '@/components/BgText.vue';
 import LangFlags from '@/components/LangFlags.vue';
 import LogoElement from '@/components/LogoElement.vue';
 import MainTitle from '@/components/MainTitle.vue';
+import RotatedText from '@/components/RotatedText.vue';
 import WhatLink from '@/components/WhatLink.vue';
 import WhereLink from '@/components/WhereLink.vue';
 import WhoLink from '@/components/WhoLink.vue';
@@ -13,6 +14,7 @@ import YellowCircle from '@/components/YellowCircle.vue';
     <div class="home-page">
         <BgText />
         <YellowCircle />
+        <RotatedText />
         <div class="home-page__content">
             <header class="flex">
                 <LangFlags />
