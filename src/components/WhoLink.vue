bd128762572d4556b9e5d7e23c9438c839bf9774
roman hrytsiuk <rmngrck97@gmail.com>
2024-12-16 15:13:18 +0200

    Updates

--- src/components/WhoLink.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang='ts'>
-</script>
-<template>
-    <RouterLink class="who__link text-center" :to="{name: 'some-page'}">who?</RouterLink>
-</template>
-<style scoped lang='scss'>
-</style>
